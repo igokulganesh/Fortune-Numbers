500630ee8c2dbdad6aed4ef6d61c2671936d867a
Gokul Ganesh I <igokulganesh@gmail.com>
2025-10-03 16:35:34 +0530

    Added PWA

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-export default defineConfig({
-  base: "/Fortune-Numbers/",
-
-  plugins: [react(), VitePWA(manifestForPlugIn)],
-
-  build: {
-    outDir: "docs",
-    emptyOutDir: false,
-    chunkSizeWarningLimit: 2000,
-  },
-
-  server: {
-    port: 3000,
-  },
-});
-
 const manifestForPlugIn = {
   registerType: "autoUpdate",
+  includeAssets: [
+    "icons/favicon.ico",
+    "icons/Icon-180.jpg",
+    "icons/Icon-512.png",
+  ],
   manifest: {
     name: "Fortune Numbers",
     short_name: "Numerology App",
@@ -27,35 +16,46 @@ const manifestForPlugIn = {
       "Discover your life's blueprint and future insights with our free numerology app. Find your fortune numbers today!",
     icons: [
       {
-        src: "/icons/Icon-192.png",
+        src: "icons/Icon-192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "favicon",
+        purpose: "any",
       },
       {
-        src: "/icons/Icon-512.png",
+        src: "icons/Icon-512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "favicon",
+        purpose: "any",
       },
       {
-        src: "/icons/Icon-180.png",
+        src: "icons/Icon-180.jpg",
         sizes: "180x180",
         type: "image/png",
-        purpose: "apple touch icon",
-      },
-      {
-        src: "/icons/Icon-512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any maskable",
+        purpose: "any",
       },
     ],
     theme_color: "#0b1220",
     background_color: "#010c26",
     display: "standalone",
-    scope: "/",
-    start_url: "/",
+    scope: "/Fortune-Numbers/",
+    start_url: "/Fortune-Numbers/",
     orientation: "portrait",
+    lang: "en",
   },
 };
+
+export default defineConfig({
+  base: "/Fortune-Numbers/",
+
+  plugins: [react(), VitePWA(manifestForPlugIn)],
+
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 2000,
+  },
+
+  server: {
+    port: 3000,
+  },
+});
